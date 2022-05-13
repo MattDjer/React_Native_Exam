e13ecf6b40144c711f11e1c03b61b9fedf730b29
Magnus <752zombie@gmail.com>
2022-05-13 23:51:43 +0200

    progress on image upload

--- components/ImagePickerExample.tsx
+++ components/ImagePickerExample.tsx
@@ -1,9 +1,35 @@
 import React, { useState, useEffect } from 'react';
 import { Button, Image, View, Platform } from 'react-native';
+import { getStorage, ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
 import * as ImagePicker from 'expo-image-picker';
+import { useDispatch } from 'react-redux';
+import { async } from '@firebase/util';
+
+function uploadImageAsync(uri : string) {
+    return async function(dispatch : any, getState : any) {
+        console.log(1);
+        const response = await fetch(uri);
+        console.log(2);
+        const blob = await response.blob();
+        console.log(3);
+        const storage = getStorage();
+        console.log(4);
+        const imageRef = ref(storage, "test.png");
+        console.log(5);
+  
+        uploadBytesResumable(imageRef, blob).then((snapshot) => {
+          console.log('Uploaded a blob or file!');
+        })
+
+        console.log(6);
+    }
+}
 
 export default function ImagePickerExample() {
   const [image, setImage] = useState<string | null>(null);
+  const dispatch = useDispatch();
+
+
 
   const pickImage = async () => {
     // No permissions request is necessary for launching the image library
@@ -18,6 +44,7 @@ export default function ImagePickerExample() {
 
     if (!result.cancelled) {
       setImage(result.uri);
+      //uploadImageAsync(result.uri);
     }
   };
 
@@ -25,6 +52,7 @@ export default function ImagePickerExample() {
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
       <Button title="Pick an image from camera roll" onPress={pickImage} />
       {image && <Image source={{ uri: image }} style={{ width: 200, height: 200 }} />}
+      <Button title='upload to server' onPress={() => dispatch(uploadImageAsync(image))}/>
     </View>
   );
 }
