922ccb0f2b414aaeab337ae0450529afc1d63591
Magnus <752zombie@gmail.com>
2022-05-19 19:19:30 +0200

    missed a spot

--- App.tsx
+++ App.tsx
@@ -3,7 +3,6 @@ import { Provider } from 'react-redux';
 import { applyMiddleware, combineReducers, createStore } from 'redux';
 import ReduxThunk from 'redux-thunk';
 import Navigation from './components/Navigation';
-import chatReducer from './store/reducers/chat.reducer';
 import postReducer from './store/reducers/post.reducer';
 import userReducer from './store/reducers/user.reducer';
 import commentReducer from './store/reducers/comment.reducer';
@@ -14,7 +13,6 @@ import eventReducer from './store/reducers/event.reducer';
 
 const rootReducer = combineReducers({
   comment: commentReducer,
-  chat: chatReducer,
   user: userReducer,
   post: postReducer,
   profile: profileReducer,
